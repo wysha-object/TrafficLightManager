c515856712ebd71551197fe4bbc46fb6070d5864
wysha-object <StarsSail@outlook.com>
2026-03-30 20:23:53 +0800

    fix: UI not loading

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -3,11 +3,6 @@ import { ModRegistrar } from "cs2/modding";
 import App from './app';
 
 const register: ModRegistrar = (moduleRegistry) => {
-  moduleRegistry.append("Menu", () => (
-    <div>
-      <h1>test123456</h1>
-    </div>
-  ))
   moduleRegistry.append("GameTopLeft", () => (
     <div id="c2vm-tle" style={{margin: 0}}>
       <App />
