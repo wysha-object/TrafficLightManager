1d8945a2990875c9895632ad762e6ad1058d16cf
wysha-object <starssail@outlook.com>
2026-04-28 22:07:01 +0800

    i18n

--- i18n-format.js
+++ i18n-format.js
@@ -10,7 +10,7 @@ const fallbackLocalizationMap = {
 console.log('Start processing localization files');
 
 const files = fs.readdirSync(i18nDir);
-const localizationMap = new Map(files
+let localizationMap = new Map(files
     .filter(filePath => filePath.endsWith('.json'))
     .map(filePath => {
         const baseName = path.basename(filePath, '.json');
@@ -28,32 +28,55 @@ const localizationMap = new Map(files
 );
 
 const baseTranslation = localizationMap.get(baseLocalization);
-localizationMap.forEach((translation, localization) => {
-    if (localization === baseLocalization) {
-        return;
-    }
-
-    for (const key of Object.keys(baseTranslation).concat(Object.keys(translation))) {
-        if (!Object.hasOwn(baseTranslation, key)) {
-            delete translation[key];
-            console.log(`REMOVE: Removed extra key "${key}" from localization "${localization}".`);
-        } else if (!Object.hasOwn(translation, key)) {
-            let target = baseLocalization;
-            for (const prefix of Object.keys(fallbackLocalizationMap)) {
-                if (localization.startsWith(prefix)) {
-                    target = fallbackLocalizationMap[prefix];
-                    break;
-                }
-            }
-
-            translation[key] = localizationMap.get(target)[key];
-            console.log(`ADD: Added missing key "${key}" to localization "${localization}" from "${target}".`);
+localizationMap = new Map(
+    localizationMap.entries().map(([localization, oldTranslation]) => {
+        if (localization === baseLocalization) {
+            return [localization, oldTranslation];
         }
-    }
-});
+
+        let translation = Object.assign({}, oldTranslation);
+
+
+        for (const key of Object.keys(baseTranslation).concat(Object.keys(translation))) {
+            if (!Object.hasOwn(baseTranslation, key)) {
+                delete translation[key];
+                console.log(`REMOVE: Removed extra key "${key}" from localization "${localization}".`);
+            } else if (!Object.hasOwn(translation, key)) {
+                let target = baseLocalization;
+                for (const prefix of Object.keys(fallbackLocalizationMap)) {
+                    if (localization.startsWith(prefix)) {
+                        let newTarget = fallbackLocalizationMap[prefix];
+                        if (Object.hasOwn(localizationMap.get(newTarget), key)) {
+                            target = newTarget;
+                        }
+                        break;
+                    }
+                }
+
+                translation[key] = localizationMap.get(target)[key];
+                console.log(`ADD: Added missing key "${key}" to localization "${localization}" from "${target}".`);
+            }
+        }
+
+        return [localization, translation];
+    })
+);
 
 localizationMap.forEach((translation, localization) => {
-    fs.writeFileSync(path.join(i18nDir, `${localization}.json`), JSON.stringify(translation, null, 4), 'utf-8');
+    fs.writeFileSync(
+        path.join(i18nDir, `${localization}.json`),
+        JSON.stringify(
+            Object.keys(translation)
+                .sort()
+                .reduce((acc, key) => {
+                    acc[key] = translation[key];
+                    return acc;
+                }, {}),
+            null,
+            4
+        ),
+        'utf-8'
+    );
 });
 
 console.log('Finish processing localization files');
