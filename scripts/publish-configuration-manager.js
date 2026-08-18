17bde08809c817fc576d1de22dd69e51d88afe99
wysha-object <starssail@outlook.com>
2026-08-19 02:35:49 +0800

    feat: update publish configuration management and dependencies

--- scripts/publish-configuration-manager.js
+++ scripts/publish-configuration-manager.js
@@ -1,4 +1,6 @@
-const fs = require('fs')
+import { XMLParser, XMLBuilder } from 'fast-xml-parser'
+import { execSync } from 'node:child_process'
+import fs from 'node:fs'
 
 const STABLE_PUBLISH_CONFIGURATION_PATH = './PublishConfigurations/Stable.xml'
 const BETA_PUBLISH_CONFIGURATION_PATH = './PublishConfigurations/Beta.xml'
@@ -14,9 +16,44 @@ if (args[0] !== 'stable' && args[0] !== 'beta') {
   process.exit(1)
 }
 
-const publishConfiguration =
+let publishConfiguration =
   args[0] === 'stable'
     ? fs.readFileSync(STABLE_PUBLISH_CONFIGURATION_PATH, 'utf-8')
     : fs.readFileSync(BETA_PUBLISH_CONFIGURATION_PATH, 'utf-8')
 
-fs.writeFileSync('./Code/Properties/PublishConfiguration.xml', publishConfiguration, 'utf-8')
+if (args[0] === 'beta') {
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+  })
+  const builder = new XMLBuilder({
+    ignoreAttributes: false,
+    format: true,
+  })
+  const parsedPublishConfiguration = parser.parse(publishConfiguration)
+
+  const now = new Date()
+  const date =
+    now.getFullYear().toString() +
+    String(now.getMonth() + 1).padStart(2, '0') +
+    String(now.getDate()).padStart(2, '0') +
+    'T' +
+    String(now.getHours()).padStart(2, '0') +
+    String(now.getMinutes()).padStart(2, '0')
+
+  const commitHash = execSync('git rev-parse HEAD').toString().trim()
+
+  parsedPublishConfiguration['Publish']['ModVersion']['@_Value'] =
+    parsedPublishConfiguration['Publish']['ModVersion']['@_Value'] +
+    '.' +
+    date +
+    '+' +
+    commitHash
+
+  publishConfiguration = builder.build(parsedPublishConfiguration)
+}
+
+fs.writeFileSync(
+  './Code/Properties/PublishConfiguration.xml',
+  publishConfiguration,
+  'utf-8',
+)
